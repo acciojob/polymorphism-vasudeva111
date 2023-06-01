abd8740e8fa7786a794b581cd3a1c7765ad3d9d2
vasudeva111 <vasudevareddy147@gmail.com>
2023-05-31 22:04:28 -0700

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
-
+//final ans
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            int pro = x * y;
+            return pro;
+        }
 
+        public int product(int x, int y, int z) {
+            int pro = x * y * z;
+            return pro;
+        }
+
+        public double product(double x, double y) {
+            double pro = x * y;
+            return pro;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(2, 2));
+        System.out.println(p.product(2, 2, 2));
+        System.out.println(p.product(2.0, 2.0));
+    }
 }
